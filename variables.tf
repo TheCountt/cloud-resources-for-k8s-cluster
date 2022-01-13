332e8e51a9276ee025b14a90c1b90e3689e41aca
TheCountt <isaacadedeji0@yahoo.com>
2022-01-13 10:16:11 +0100

    commit changes

--- variables.tf
+++ variables.tf
@@ -18,20 +18,28 @@ variable "enable_classiclink_dns_support" {}
 
 variable "resource_tag" {}
 
-variable "master_ip_list" {
-  description = "targeted ip addresses"
-  type        = list(any)
-}
-
-variable "worker_ip_list" {
-  description = "targeted ip addresses"
-  type        = list(any)
-}
-
-variable "target_id" {
-  default = ["master_ip_list"]
-}
-
 variable "instance_type" {}
 
 variable "ami" {}
+
+variable "pod_routes" {
+  default = ["172.20.0.0/24", "172.20.1.0/24", "172.20.2.0/24"]
+  description = "targeted pod ranges"
+  type = list
+}
+
+/////////
+# Not part of code
+# variable "master_ip_list" {
+#   description = "targeted ip addresses"
+#   type        = list(any)
+# }
+
+# variable "worker_ip_list" {
+#   description = "targeted ip addresses"
+#   type        = list(any)
+# }
+
+# variable "target_id" {
+#   default = ["master_ip_list"]
+# }
